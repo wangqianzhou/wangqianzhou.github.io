f608077d5926f0ce90eaf5caee7db01a8cfc74a0
wangqianzhou <wangqianzhou@foxmail.com>
2017-08-15 11:33:08 +0800

    add error demo

---
+++ worker-simple-demo/serviceworker_error.js
@@ -0,0 +1,14 @@
+// Install is called when the page is loaded first time, or when the service
+self.addEventListener('install', function(event) {
+    console.log('Installing Service Worker');
+
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Received fetch');
+    console.log(event);
+});
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
