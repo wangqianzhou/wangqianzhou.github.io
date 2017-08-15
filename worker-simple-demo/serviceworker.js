22b0229739e78d040691a23b2107862ca990ad97
wangqianzhou <wangqianzhou@foxmail.com>
2017-08-15 11:21:42 +0800

    add demo

---
+++ worker-simple-demo/serviceworker.js
@@ -0,0 +1,24 @@
+// Install is called when the page is loaded first time, or when the service
+self.addEventListener('install', function(event) {
+    console.log('Installing Service Worker');
+
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Received fetch');
+    console.log(event);
+
+    if (/\.jpg$/.test(event.request.url)) {
+        console.log('Replace image!');
+
+        event.respondWith(
+            fetch('dog.jpeg', {
+                mode: 'no-cors'
+            })
+        );
+    }
+});
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
